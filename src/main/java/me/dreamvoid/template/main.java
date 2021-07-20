363ac1417774f0cd5deafcdf5fa6c34c620ae7d7
流光溢彩Last <dreamvoid@dreamvoid.ml>
2021-07-20 16:03:16 +0800

    更新代码

--- src/main/java/me/dreamvoid/template/main.java
+++ src/main/java/me/dreamvoid/template/main.java
@@ -1,37 +1,43 @@
 package me.dreamvoid.template;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import org.bstats.bukkit.Metrics;
 
 import java.io.File;
 
-public class main extends JavaPlugin {
-
-    // 定义变量
+public class main extends JavaPlugin implements Listener {
     public static YamlConfiguration config;
 
-    @Override
-    public void onLoad() { } // 加载插件
+    @Override // 加载插件
+    public void onLoad() {
 
-    @Override
-    public void onEnable() { LoadConfig(); } // 启用插件
+    }
 
-    @Override
-    public void onDisable() { } // 禁用插件
+    @Override // 启用插件
+    public void onEnable() {
+        LoadConfig();
+        Bukkit.getPluginManager().registerEvents(this,this); // 注册事件监听
+        getCommand("some-command").setExecutor(this); // 注册插件命令
+    }
+
+    @Override // 禁用插件
+    public void onDisable() {
+
+    }
 
     private void LoadConfig() {
         // 加载配置文件
         File configure = new File(getDataFolder(), "config.yml");
         if(!(configure.exists())){ saveDefaultConfig(); }
         config = YamlConfiguration.loadConfiguration(configure);
-
-        // bStats统计
-        if(config.getBoolean("bStats", true)) {
-            int pluginId = 1234;
-            Metrics metrics = new Metrics(this, pluginId);
-        }
     }
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return super.onCommand(sender, command, label, args);
+    }
 }
